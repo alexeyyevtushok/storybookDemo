36d140f626f4ea217539fcf0d528f7e70133938f
Alexey Yevtushok <alexey.yevtushok@gmail.com>
2020-02-17 18:38:26 +0200

    publish

--- src/components/Button/src/Button.tsx
+++ src/components/Button/src/Button.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import './Button.css';
-
-declare interface ButtonProps {
-    textValue?: string
-    classValue?: string
-}
+import { ButtonProps } from './Button.d';
 
 const Button = ({textValue, classValue}: ButtonProps) => {
     const setClass = classValue || 'default'
