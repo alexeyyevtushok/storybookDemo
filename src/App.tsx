d5b5f4b6ce5b5ebd6401b94aa7254edd7fb9aa3f
Alexey Yevtushok <alexey.yevtushok@gmail.com>
2020-02-17 18:22:43 +0200

    npm published

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Button from "./components/Button/Button";
+
+const App = () => {
+  return (
+    <div className="App">
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/src/Button.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Button.css';
+import { ButtonProps } from './Button.d';
+
+const Button = ({textValue, classValue}: ButtonProps) => {
+    const setClass = classValue || 'default'
+    const [ active, setActive ] = React.useState(false);
+    return (
+        <div className="App">
+            <button className={setClass} onClick={() => setActive(!active)}>{textValue || 'click'}</button>
+            <br/>
+            {active && <span>Button is clicked</span>}
+        </div>
+    );
+}
+
+export default Button;
